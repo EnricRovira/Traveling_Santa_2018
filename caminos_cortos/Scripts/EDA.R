75c14c8f753325c06b465d5955a7a2c846c77069
WungusMax <enric.rovira96@gmail.com>
2018-11-29 22:07:16 +0100

    finish EDA.r script and add python notebook

--- caminos_cortos/Scripts/EDA.R
+++ caminos_cortos/Scripts/EDA.R
@@ -32,12 +32,23 @@ cities %>% select(Prime) %>% summarize(Total = sum(Prime), Proportion=mean(Prime
 #We can see that almost 10% of the cities (17,802) are "primes". Since we need a prime city each 10th step, this could be a good thing.
 
 # Let's plot out dataset again but including the Prime cities.
+
 cond <- ifelse(cities$Prime==1, "red", "black") # a condition to make the prime cities points colour red.
 cities %>% 
   ggplot(aes(x=X, y=Y))+
   geom_point(shape = ".", alpha="0.75", colour=cond)
 
 #Let´s take a look only to Prime cities
+
+cond2 <- ifelse(cities$Prime==1, "red", "white")
+cities %>% 
+  ggplot(aes(x=X, y=Y))+
+  geom_point(shape = ".", alpha="1", colour=cond2)
+
+#Prime cities not only correspond to (almost) 10% of the cities but they also seem to be very well distributed along the "map"
+
+####---###
+
 
 
 
--- caminos_cortos/main.py
+++ caminos_cortos/main.py
@@ -1,5 +1,8 @@
 import numpy as np
-import pandas as pc
+import pandas as pd
 import matplotlib.pyplot as plt
 #import seaborn as sns
 import random
+
+df_cities = pd.read_csv('data/cities.csv')
+df_cities.head()
